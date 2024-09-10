9d8bd878df456bd6622e29daded15fc99913d2f9
samuel-cavazos <samuel.cavazos@dhr-rgv.com>
2024-09-10 15:22:41 -0500

    testing import styling

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+from .py835 import parse  # Assuming py835.py contains the parse function
+
+__all__ = ['parse']  # Optionally, you can define __all__ to explicitly specify exported functions
